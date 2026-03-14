9d2eb3668eca9cd317c2804c31c6168ff45e5774
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:57:44 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:40.519Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T22:57:44.256Z
+Commit: Fix memory leak in event handlers
