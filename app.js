ea4c9331d6b870ce1b217ed7453d716105bb1d52
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:57:07 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:03.976Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-14T22:57:06.983Z
+Commit: Fix broken links in documentation
