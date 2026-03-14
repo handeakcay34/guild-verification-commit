d6ebcb8873b0a05726e0deec56a1c0bcae5617d8
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:58:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:58:08.527Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T22:58:15.568Z
+Commit: Fix broken links in documentation
