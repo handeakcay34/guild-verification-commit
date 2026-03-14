a4a05741e6af91cde95dd16fa2f75189751e5f20
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:58:00 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:55.165Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T22:58:00.360Z
+Commit: Fix broken links in documentation
