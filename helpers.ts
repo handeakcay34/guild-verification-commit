f273b5b4d6d1c0aff541e5bdb4ec5813770683f6
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:57:21 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:44:57.877Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T22:57:21.276Z
+Commit: Add multi-language support
