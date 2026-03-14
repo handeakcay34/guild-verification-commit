506674e0366fec2fa287ae9bb5232beb96de7de0
handeakcay34 <handeakcay34@hotmail.com>
2026-03-15 01:57:51 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:41.473Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T22:57:51.129Z
+Commit: Fix memory leak in event handlers
